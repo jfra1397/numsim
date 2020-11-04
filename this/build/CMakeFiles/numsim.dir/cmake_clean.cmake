69ea4abe32167a9132c3e9eb1db5224e23c5cc46
jfra1397 <70207821+jfra1397@users.noreply.github.com>
2020-11-04 16:27:43 +0100

    Add files via upload

---
+++ this/build/CMakeFiles/numsim.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/numsim.dir/this/src/main.cpp.o"
+  "../bin/numsim.pdb"
+  "../bin/numsim"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/numsim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
