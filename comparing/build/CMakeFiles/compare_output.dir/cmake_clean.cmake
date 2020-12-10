3bd67d62aeb54a2b4277f826fa984620264ece68
jfra1397 <jfra@hotmail.de>
2020-12-10 14:54:05 +0100

    blub

---
+++ comparing/build/CMakeFiles/compare_output.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/compare_output.dir/main.cpp.o"
+  "compare_output"
+  "compare_output.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/compare_output.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
